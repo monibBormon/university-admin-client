3f782f68d5f0e0c78f2bcf593e406501ea89ca47
monibBormon <monibbormon14@gmail.com>
2021-12-27 13:32:17 +0600

    pagination added

--- src/pages/DashboardPage/AllCourses/AllCourses.js
+++ src/pages/DashboardPage/AllCourses/AllCourses.js
@@ -1,33 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './AllCourses.css'
 
 const AllCourses = () => {
     const [courses, setCourses] = useState([])
-    const [isDelete, setIsDelete] = useState(null)
+    const [pageCount, setPageCount] = useState(0)
+    const [page, setPage] = useState(0)
 
+    const pageSize = 5
     useEffect(() => {
-        fetch('http://localhost:5000/courses')
+        fetch(`http://localhost:5000/courses?page=${page}&&size=${pageSize}`)
             .then(res => res.json())
-            .then(data => setCourses(data))
-    }, [isDelete])
-    // delete product
-    const handleDeleteProduct = (id) => {
-        const confirmation = window.confirm('Are you sure product will be delete parmanently?')
-        if (confirmation) {
-            fetch(`http://localhost:5000/delete/${id}`, {
-                method: 'DELETE',
-                headers: { 'content-type': 'application/json' }
-            }).then(res => res.json())
-                .then(data => {
-                    if (data.deletedCount) {
-                        setIsDelete(true)
-                        alert('Deleted Successfully.')
-                    } else {
-                        setIsDelete(false)
-                    }
-                })
-        }
-    }
+            .then(data => {
+                setCourses(data.result)
+                const count = data.count;
+                const pageNumber = Math.ceil(count / pageSize)
+                setPageCount(pageNumber)
+            })
+    }, [page])
+
     return (
         <div>
             <h2 className='text-center text-4xl font-semibold'>All Student List</h2>
@@ -35,51 +26,25 @@ const AllCourses = () => {
                 <div className="flex flex-col">
                     <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                <table className="min-w-full divide-y divide-gray-200">
-                                    <thead className="bg-gray-50">
-                                        <tr>
-                                            <th
-                                                scope="col"
-                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                            >
-                                                Serial No.
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                            >
-                                                Student Name
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                            >
-                                                Action
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="bg-white divide-y divide-gray-200">
-                                        {courses.map((course, index) => (
-                                            <tr key={course._id}>
-                                                <td className="px-6 py-4 whitespace-nowrap">
-                                                    <div className="text-sm text-gray-900 capitalize">{index + 1}</div>
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap">
-                                                    <div className="text-sm text-gray-900 capitalize">{course.name}</div>
-                                                </td>
-                                                <td className="px-6 py-4 whitespace-nowrap">
-                                                    <Link to={`/student-details/${course._id}`}>
-                                                        <button className='border-2 border-red-500 rounded-full px-2 mr-1'>View Details</button>
-                                                    </Link>
-                                                    <div onClick={() => handleDeleteProduct(course._id)} className="text-sm text-gray-900 capitalize inline-block"><button className='border-2 border-red-500 rounded-full px-2 mr-1'>Delete Student</button>
-                                                    </div>
-
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
+                            <div className="overflow-hidden">
+                                {courses.map((course, index) => (
+                                    <div key={course._id} className='flex shadow p-3 mb-3'>
+                                        <img src={course.img} className='w-20 rounded' alt="" />
+                                        <div className='ml-5'>
+                                            <h3 className='text-2xl mb-2'>{course.name}</h3>
+                                            <Link to={`/student-details/${course._id}`}>
+                                                <button className='border-2 border-red-500 rounded-full px-2 text-sm mr-1'>View Details</button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                ))}
+                                <div className="pagination">
+                                    {
+                                        [...Array(pageCount).keys()].map(number => <button className={number === page ? 'selected' : ''} key={number}
+                                            onClick={() => setPage(number)}
+                                        >{number + 1}</button>)
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
